2b117b58c534622757405f82bccf3a5fddd140c5
Karan Govil <karangovil@gmail.com>
2019-09-01 17:01:03 -0400

    update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
-import Lib
 import Transformers
+import qualified Data.Map as Map
 
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 exampleExpBad = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Var "x" `Plus` Lit 2))
 exampleExpBad2 = Plus (Lit 12) (Abs "x" (Var "x"))
 
 main :: IO ()
-main = someFunc
+main = do
+    print $ runEval4 Map.empty 0 (eval4 exampleExp)
